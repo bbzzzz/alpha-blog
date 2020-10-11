040cd95ad6bacd1891498eda9845964c100c1b3e
Bohan Zhang <zhangbohan1990@gmail.com>
2020-10-10 17:20:12 -0700

    flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
+      flash[:notice] = 'Article was created successfully.'
       redirect_to @article
     else
       render 'new'
     end
+  end
+
+  def update
   end
 end
