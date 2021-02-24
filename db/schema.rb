a6051f6b851ca9d202f588b4d046cd89fdd50ed6
Bohan Zhang <zhangbohan1990@gmail.com>
2021-02-23 21:35:34 -0800

    change column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_033232) do
+ActiveRecord::Schema.define(version: 2021_02_24_033929) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_033232) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
